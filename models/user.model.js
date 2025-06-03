f80e9f90ff15221b7e78d00b4f2fa82de2e18785
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-06-03 17:02:08 +0530

    user model created

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+
+export const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role : {
+        type: String,
+        enum: ['student', 'teacher'],
+        required: true
+    }
+},{timestamps: true})
+
+//password middleware
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
